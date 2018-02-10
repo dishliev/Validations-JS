03ff951132426d06ac69d5ed50136dac1b47cdfd
Kiril Dishliev <kiril.dishliev1@gmail.com>
2018-02-10 20:15:42 +0200

    ip

--- validations.js
+++ validations.js
@@ -1,37 +1,41 @@
 ﻿var Regex = {
     Email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    IsLetterAndNumber: /^[0-9a-zA-Z]+$/,
-    IsLetter: /^[A-Za-z]+$/,
-    IsNumber: /^\d+$/,
+    LetterAndNumber: /^[0-9a-zA-Z]+$/,
+    Letter: /^[A-Za-z]+$/,
+    Number: /^\d+$/,
     ContainNumber: /[0123456789]/,
-    IsDate: /^(0?[1-9]|1[012])[\/](0?[1-9]|[12][0-9]|3[01])[\/]\d{4}$/, // MM/DD/YYYY and M/D/YYYY
+    Date: /^(0?[1-9]|1[012])[\/](0?[1-9]|[12][0-9]|3[01])[\/]\d{4}$/, // MM/DD/YYYY and M/D/YYYY
     Url: /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+    IP: /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i
 };
 
 var Validate = {
     Email: function (email) {
         return Regex.Email.test(email);
     },
-    IsNumber: function (number) {
-        return Regex.IsNumber.test(number);
+    Number: function (number) {
+        return Regex.Number.test(number);
     },
     NumberIsBetween: function (number, number_from, number_to) {
         return number >= number_from <= number_to;
     },
-    IsLetterAndNumber: function (val) {
-        return Regex.IsLetterAndNumber.test(val);
+    LetterAndNumber: function (val) {
+        return Regex.LetterAndNumber.test(val);
     },
-    IsLetter: function (val) {
-        return Regex.IsLetter.test(val);
+    Letter: function (val) {
+        return Regex.Letter.test(val);
     },
     ContainNumber: function (val) {
         return Regex.ContainNumber(val);
     },
-    IsDate: function (date) {
-        return Regex.IsDate.test(date);
+    Date: function (date) {
+        return Regex.Date.test(date);
     },
     Url: function (url) {
         return Regex.Url.test(url);
+    },
+    IP: function(ip){
+        return Regex.IP.test(ip);
     }
 
 };
