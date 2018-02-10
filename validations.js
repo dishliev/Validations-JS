253db2722d078ec1eee5140eb7162b7687648321
Kiril Dishliev <kiril.dishliev1@gmail.com>
2018-02-10 20:07:05 +0200

    isDate

--- validations.js
+++ validations.js
@@ -27,53 +27,8 @@ var Validate = {
         return Regex.ContainNumber(val);
     },
     IsDate: function (date) {
-        var year = function () {
-
-        };
-
-        this.format = function (format) {
-            if (format.length == 8) {
-                var f = format.split();
-                var separator = null;
-
-                if (!/[dDmMyY]/.test(f[2])) {
-                    separator = f[2];
-                }
-                else if (!/[dDmMyY]/.test(f[4]) && /[dDmMyY]/.test(f[2])) {
-                    separator = f[4];
-                }
-
-                var re = [/dD/, /mM/, /yY/];
-                
-                var d = re[0].test(f[0]);
-                var m = re[1].test(f[3]);
-                var y = re[2].test([6]);
-                var a = re[2].test([0]);
-
-                if (d && m && y) {
-                    //ddmmyyyy
-                }
-                else if (!d && !m && y) {
-                    //mmddyyyy
-                }
-                else if (!d && !m && !y && a) {
-                    //yyyyddmm
-                }
-                else if (!d && m && !y) {
-                    //yyyymmdd
-                }
-                else if (d && !m && !y) {
-                    //ddyyyymm
-                }
-            }
-            
-        }
-        if (this.format.length != 0) {
-            this.format.call;
-        }
-        return this;
-    },
-
+        return Regex.IsDate.test(date);
+    }
 };
 
 
